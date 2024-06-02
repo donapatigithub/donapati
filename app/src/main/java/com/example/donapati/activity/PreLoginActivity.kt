c8b1a3d5b9e3c8bb4024c84739ce0f12efee0b52
donapatigithub <donapatisivareddy8@gmail.com>
2024-06-02 19:56:14 +0530

    main commit

---
+++ app/src/main/java/com/example/donapati/activity/PreLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.donapati.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.donapati.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pre_login_activity)
+    }
+}
