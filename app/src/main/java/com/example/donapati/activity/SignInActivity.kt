c18e4499e69a0d87e416708a1572fa49959b1344
donapatigithub <donapatisivareddy8@gmail.com>
2024-06-06 12:37:46 +0530

    SignIn Done

--- app/src/main/java/com/example/donapati/activity/SignInActivity.kt
+++ app/src/main/java/com/example/donapati/activity/SignInActivity.kt
@@ -1,21 +1,26 @@
 package com.example.donapati.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.donapati.R
+import com.example.donapati.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onClick()
+    }
+    fun onClick(){
+        binding.signUpTextView.setOnClickListener {
+            startActivity(Intent(this@SignInActivity,SignUpActivity::class.java))
         }
     }
 }
