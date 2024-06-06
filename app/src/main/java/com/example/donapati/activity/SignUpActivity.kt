2f763f131751a77794bd1563089d8ba52eca9c58
donapatigithub <donapatisivareddy8@gmail.com>
2024-06-06 13:01:41 +0530

    Minor changes

--- app/src/main/java/com/example/donapati/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/donapati/activity/SignUpActivity.kt
@@ -16,7 +16,7 @@ import android.util.Log
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
-    private val READ_CONTACTS_PERMISSION_REQUEST = 1
+    private val GET_ACCOUNTS_PERMISSION_REQUEST = 1
     private val TAG = "SignUpActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,14 +40,15 @@ class SignUpActivity : AppCompatActivity() {
                 startActivity(Intent(this@SignUpActivity, PreLoginActivity::class.java))
             }
             googleButton.setOnClickListener {
-                openApp("com.google")
+                openApp("com.google.android.apps.gmail")
             }
             facebookButton.setOnClickListener {
                 openApp("com.facebook.katana")
             }
 
-            if (ContextCompat.checkSelfPermission(this@SignUpActivity, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this@SignUpActivity, arrayOf(Manifest.permission.READ_CONTACTS), READ_CONTACTS_PERMISSION_REQUEST)
+            // Request GET_ACCOUNTS permission
+            if (ContextCompat.checkSelfPermission(this@SignUpActivity, Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this@SignUpActivity, arrayOf(Manifest.permission.GET_ACCOUNTS), GET_ACCOUNTS_PERMISSION_REQUEST)
             } else {
                 getGoogleAccounts()
             }
@@ -84,11 +85,10 @@ class SignUpActivity : AppCompatActivity() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == READ_CONTACTS_PERMISSION_REQUEST) {
+        if (requestCode == GET_ACCOUNTS_PERMISSION_REQUEST) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 getGoogleAccounts()
             } else {
-                // Handle permission denial (e.g., show a message)
                 Toast.makeText(this, "Permission denied to access accounts", Toast.LENGTH_SHORT).show()
             }
         }
